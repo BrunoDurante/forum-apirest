0a012391c0ab9d841bf43697dcf00bfbf1ebc8ba
Bruno Durante <brunodurantec@gmail.com>
2022-08-08 22:45:14 -0300

    Configurando projeto com spring initializer io

---
+++ src/main/kotlin/br/com/dutech/forum/HelloController.kt
@@ -0,0 +1,16 @@
+package br.com.dutech.forum
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/hello")
+class HelloController {
+
+    @GetMapping
+    fun showHello(): String{
+        return "Hello World! s2"
+    }
+
+}
