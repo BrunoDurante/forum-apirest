0acd1c68ffba3e087094323151e9cee377420d65
Bruno Durante <brunodurantec@gmail.com>
2022-09-17 23:02:47 -0300

    Incluindo tratamento de erro bean validation

--- src/main/kotlin/br/com/dutech/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/dutech/forum/exception/ExceptionHandler.kt
@@ -2,6 +2,7 @@ package br.com.dutech.forum.exception
 
 import br.com.dutech.forum.dto.ErrorView
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -22,6 +23,24 @@ class ExceptionHandler {
             message = exception.message,
             path = request.servletPath
 
+        )
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handleValidationError(
+        exception: MethodArgumentNotValidException,
+        request: HttpServletRequest
+    ): ErrorView {
+        val errorMessage: HashMap<String, String?> = HashMap()
+        exception.bindingResult.fieldErrors.forEach{
+            e -> errorMessage.put(e.field, e.defaultMessage)
+        }
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
+            message = errorMessage.toString(),
+            path = request.servletPath
         )
     }
 
